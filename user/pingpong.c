09a8f7d518b1172cfb2f804966d170b0f0faa703
suzumiya_haruhi <suzumiya@haruhi.com>
2024-11-06 19:40:42 +0800

    pingpong完成

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,5 +4,39 @@
 
 int main(int argc,char *argv[])
 {
-    
+    if(argc != 1){
+        fprintf(2,"Error: pingpong failed, check something...\n");
+        exit(1);
+    }
+    int p_to_c[2],c_to_p[2];
+    pipe(p_to_c);
+    pipe(c_to_p);
+    char x = 'x';
+    int pid;
+    if(pid = fork() == -1){
+        fprintf(2,"Error: fork...\n");
+        exit(1);
+    }
+    if(pid == 0){
+        close(p_to_c[1]);
+        close(c_to_p[0]);
+        if(read(p_to_c[0],&x,sizeof(x))>0){
+            printf("%d: received ping\n",pid);
+        }
+        if(write(c_to_p[1],&x,sizeof(x) >= 0)){
+
+        }
+    }else{
+        close(p_to_c[0]);
+        close(c_to_p[1]);
+        if(write(p_to_c[1],&x,sizeof(x)) >= 0){
+
+        }
+        if(read(c_to_p[0],&x,sizeof(x)) > 0){
+            printf("%d: received pong\n",pid);
+        }
+    }
+    close(p_to_c);
+    close(c_to_p);
+    exit(0);
 }
