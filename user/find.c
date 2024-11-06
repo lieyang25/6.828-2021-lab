854505664709c453f8454fad120fa5d3aad14bc9
suzumiya_haruhi <suzumiya@haruhi.com>
2024-11-06 22:18:56 +0800

    混乱

--- user/find.c
+++ user/find.c
@@ -5,5 +5,48 @@
 
 int main(int argc,char* argv[])
 {
+    
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
 
+    if((fd = open(path, 0)) < 0){
+        fprintf(2, "ls: cannot open %s\n", path);
+        return;
+    }
+
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "ls: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
+    switch(st.type){
+    case T_FILE:
+        printf("./\n", fmtname(path), st.type, st.ino, st.size);
+        break;
+
+    case T_DIR:
+        if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+            printf("ls: path too long\n");
+            break;
+        }
+        strcpy(buf, path);
+        p = buf+strlen(buf);
+        *p++ = '/';
+        while(read(fd, &de, sizeof(de)) == sizeof(de)){
+        if(de.inum == 0)
+            continue;
+        memmove(p, de.name, DIRSIZ);
+        p[DIRSIZ] = 0;
+        if(stat(buf, &st) < 0){
+            printf("ls: cannot stat %s\n", buf);
+            continue;
+        }
+        printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+    }
+    break;
+  }
+    close(fd);
+    exit(0);
 }
